f9063700f39d4afb597fc4750d0fb3b089ff40f2
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-11 06:01:17 -0700

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <img src='https://robohash.org/test?200x200' alt='machine' />
+      <div>
+        <h2>Jane Doe</h2>
+        <h2>jane.doe@gmail.com</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
