0be9e35a7b4138c9765d256c7accefbc49728d9e
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-12 02:19:34 -0700

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,58 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Bret',
+    email: 'sincere@april.biz',
+  },
+  {
+    id: 2,
+    name: 'Bret',
+    email: 'sincere@april.biz',
+  },
+  {
+    id: 1,
+    name: 'Bret',
+    email: 'sincere@april.biz',
+  },
+  {
+    id: 1,
+    name: 'Bret',
+    email: 'sincere@april.biz',
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'sincere@april.biz',
+  },
+  {
+    id: 6,
+    name: 'Mrs Dennis Schulist',
+    username: 'leopoldo_colkery',
+    email: 'sincere@april.biz',
+  },
+  {
+    id: 7,
+    name: 'Kurtis weissnat',
+    username: 'Elwin.Skiles',
+    email: 'sincere@april.biz',
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir',
+    username: 'Maxime_nienow',
+    email: 'sincere@april.biz',
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'sincere@april.biz',
+  },
+  {
+    id: 10,
+    name: 'Clementina Dubuque',
+    username: 'Moriah.Stanton',
+    email: 'sincere@april.biz',
+  },
+];
