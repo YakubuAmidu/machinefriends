2f7d1b5da959dbd5c04e4b52a83201ce8a3486a5
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-12 02:44:25 -0700

    Initial commit

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = (props) => {
   return (
-    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src='https://robohash.org/test?200x200' alt='machine' />
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img src={`https://robohash.org/${props.id}?200x200`} alt='machine' />
       <div>
         <h2>{props.name}</h2>
         <h2>{props.email}</h2>
