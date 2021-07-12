310afabae57b1c391f1c9913e6c874b69516ab7c
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-12 04:50:16 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,25 @@ class App extends Component {
     super();
     this.state = {
       robots: robots,
-      Searchfield: '',
+      searchfield: '',
     };
   }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
   render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
     return (
       <div className='tc'>
         <h1>MachineFriends</h1>
-        <SearchBox />
-        <CardList robots={this.state.robots} />
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
